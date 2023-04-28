bba2e7454f065bd9cccf617eb3c98b6a3c95e24d
iamafasha <afashaisakiye@gmail.com>
2023-04-28 23:12:07 +0300

    Add traccar database config

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'traccar' => [
+            'driver' => 'mysql',
+            'url' => env('TRACCAR_DATABASE_URL'),
+            'host' => env('TRACCAR_DB_HOST', '127.0.0.1'),
+            'port' => env('TRACCAR_DB_PORT', '3306'),
+            'database' => env('TRACCAR_DB_DATABASE', 'traccar'),
+            'username' => env('TRACCAR_DB_USERNAME', 'traccar_user'),
+            'password' => env('TRACCAR_DB_PASSWORD', ''),
+            'unix_socket' => env('TRACCAR_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
