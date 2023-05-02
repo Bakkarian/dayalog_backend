aef10f881d2e07341fdc9859b2292d41aefa3646
iamafasha <afashaisakiye@gmail.com>
2023-05-02 22:31:38 +0300

    Add vehicle table

---
+++ database/migrations/2023_05_02_112001_create_vehicles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Device;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('number_plate')->unique();
+            $table->foreignIdFor(Device::class);
+            $table->string('model');
+            //TODO: add columns that represent capacity (What the vehicle can carry)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
