249d0e59519759974f7521528343599fc5992d5d
iamafasha <afashaisakiye@gmail.com>
2023-05-16 06:20:33 +0300

    Add vehicle device relationship

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'vin',
+        'number_plate',
+        'make',
+        'model',
+        'capacity_weight',
+        'capacity_volume',
+        'insurance_policy_number',
+        'insurance_coverage',
+        'insurance_expiration',
+    ];
+
+    /**
+     * Get the device associated with the vehicle.
+     * 
+     */
+    public function device()
+    {
+        return $this->hasOneThrough(
+            Device::class,
+            VehicleDevice::class,
+            'vehicle_id', // Foreign key on the "vehicle_devices" table
+            'id', // Foreign key on the "devices" table
+            'id', // Local key on the "vehicles" table
+            'device_id' // Local key on the "vehicle_devices" table
+        );
+    }
 }
