dc8e990a5a143b4bc0ffa1463540f6489e7ef25e
iamafasha <afashaisakiye@gmail.com>
2023-05-23 21:21:42 +0300

    Rename Driver Resource from Driver to DriverResource

--- app/Http/Resources/DriverResource.php
+++ app/Http/Resources/DriverResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class Driver extends ResourceCollection
+class DriverResource extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
