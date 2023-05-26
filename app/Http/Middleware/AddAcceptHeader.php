226053f699136fb5d0a7e26064c748b4042d2997
iamafasha <afashaisakiye@gmail.com>
2023-05-26 11:37:35 +0300

    Add Accept Header

---
+++ app/Http/Middleware/AddAcceptHeader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddAcceptHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the "Accept" header is not already set
+
+            $request->headers->set('Accept', 'application/json');
+
+
+        return $next($request);
+    }
+}
