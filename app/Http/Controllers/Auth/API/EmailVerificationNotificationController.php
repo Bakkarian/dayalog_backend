4e1e1e0317d6fbba7075ca104998557f636af93d
iamafasha <afashaisakiye@gmail.com>
2023-05-26 11:41:41 +0300

    Move Auth Api controllers to Auth/API

---
+++ app/Http/Controllers/Auth/API/EmailVerificationNotificationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth\API;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class EmailVerificationNotificationController extends Controller
+{
+    /**
+     * Send a new email verification notification.
+     *
+     * @group Authentication
+     */
+    public function store(Request $request): JsonResponse|RedirectResponse
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return response()->json(['status' => 'verification-link-sent']);
+    }
+}
