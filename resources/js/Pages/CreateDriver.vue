909c39342adc5d98fa67daa70443f6e2c1ed4ab0
Ivan Yiga <ivanatresyn@gmail.com>
2023-06-14 20:35:13 +0300

    Finalized the Create driver form

--- resources/js/Pages/CreateDriver.vue
+++ resources/js/Pages/CreateDriver.vue
@@ -33,6 +33,12 @@
                         </div>
                     </div>
                     <div class="sm:col-span-5">
+                        <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Drivers Licence Number *</label>
+                        <div class="mt-2">
+                            <input type="text" name="licence-number" id="phone" required placeholder="Drivers Licence Number" autocomplete="licence-number" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                    </div>
+                    <div class="sm:col-span-5">
                         <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Password *</label>
                         <div class="mt-2">
                             <input type="text" name="password" id="password" placeholder="Merchant Code" autocomplete="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
