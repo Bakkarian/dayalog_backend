00f63c5692b49f22a14cb54a2c08c9c3baf7f048
iamafasha <afashaisakiye@gmail.com>
2023-06-15 05:44:55 +0300

    fix: order vehicle relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,4 +10,9 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function vehicles()
+    {
+        return $this->belongsToMany(Vehicle::class);
+    }
 }
