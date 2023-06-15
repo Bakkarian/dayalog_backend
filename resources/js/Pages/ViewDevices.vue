7aae57665c9e154a1f499bf29ecf39bdd80b137e
Ivan Yiga <ivanatresyn@gmail.com>
2023-06-15 09:01:09 +0300

    Added ViewDevice component

---
+++ resources/js/Pages/ViewDevices.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="px-4 sm:px-6 lg:px-8 max-w-7xl">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="text-base font-semibold leading-6 text-gray-900">Devices</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all the devices registered in the system.</p>
+            </div>
+            <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                <a href="/add-device" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Add Device
+                </a>
+            </div>
+        </div>
+        <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Device Name</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Unique ID</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Model</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Position Id</th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                    <span class="sr-only">Status</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                            <tr v-for="device in devices" :key="device.model">
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ device.name }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ device.uniqueid }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ device.model }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-blue-500">
+                                    <a href="#">{{ device.positionid }}</a>
+                                </td>
+                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                    <CheckBadgeIcon v-if="device.disabled !== 0" class="h-6 w-6 text-green-500" aria-hidden="true" />
+                                    <ExclamationCircleIcon v-if="device.disabled === 0" class="h-6 w-6 text-red-500" aria-hidden="true" />
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {
+    ExclamationCircleIcon,
+    CheckBadgeIcon,
+} from '@heroicons/vue/24/solid'
+const devices = [
+    { name: 'Ivan tracker', uniqueid: '038020188471', model: 'tk103', positionid: '5981', disabled: 0 },
+    // More people...
+]
+</script>
