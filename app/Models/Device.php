6a5e89b45291944120ef22b7e6ecb431f6c07345
iamafasha <afashaisakiye@gmail.com>
2023-06-28 23:19:45 +0300

    create a different tracar device model

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,40 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Device extends Model
 {
     use HasFactory;
-
-    protected $connection = 'traccar';
-
-    protected $table = 'tc_devices';
-
-    public $timestamps = false;
-
-    protected $attributes = [
-        'attributes' => '{}',
-    ];
-
-    protected $fillable = [
-        'name',
-        'uniqueid',
-        'model'
-    ];
-
-    public static function create(array $attributes = [])
+    public function vehicle()
     {
-        $device = static::query()->create($attributes);
-
-        $connectionName = (new static())->getConnectionName();
-
-        DB::connection($connectionName)->table('tc_user_device')->insert([
-            'userid'=> $attributes['tc_user_id'] ?? 1,
-            'deviceid'=> $device->id
-        ]);
-
-        return $device;
+        return $this->hasOneThrough(
+            Vehicle::class,
+            VehicleDevice::class,
+            'device_id',// Foreign key on the VehicleDevice model
+            'id', // Foreign key on the Vehicle model
+            'id', // Local key on the Device model
+            'vehicle_id',  // Local key on the VehicleDevice model
+        );
     }
 
     //TODO: Create a custom delete that caters for the tc_user_device table
