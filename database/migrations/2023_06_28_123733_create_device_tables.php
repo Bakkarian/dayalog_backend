84f963ce8199cec923f91d93987a33e638b78502
iamafasha <afashaisakiye@gmail.com>
2023-06-29 13:46:00 +0300

    remove reverse triggers

--- database/migrations/2023_06_28_123733_create_device_tables.php
+++ database/migrations/2023_06_28_123733_create_device_tables.php
@@ -62,39 +62,6 @@ return new class extends Migration
                 DELETE FROM $devicesTable WHERE id = OLD.id;
             END
         ");
-
-        // Create trigger for INSERT operation (vice versa)
-        DB::unprepared("
-            CREATE TRIGGER dayalog_devices_insert_trigger
-            AFTER INSERT ON $devicesTable
-            FOR EACH ROW
-            BEGIN
-                INSERT INTO $tcDevicesTable (id, name, uniqueid, lastupdate, positionid, groupid, attributes, phone, model, contact, category, disabled, status, geofenceids, expirationtime, motionstate, motiontime, motiondistance, overspeedstate, overspeedtime, overspeedgeofenceid, motionstreak)
-                VALUES (NEW.id, NEW.name, NEW.uniqueid, NEW.lastupdate, NEW.positionid, NEW.groupid, NEW.attributes, NEW.phone, NEW.model, NEW.contact, NEW.category, NEW.disabled, NEW.status, NEW.geofenceids, NEW.expirationtime, NEW.motionstate, NEW.motiontime, NEW.motiondistance, NEW.overspeedstate, NEW.overspeedtime, NEW.overspeedgeofenceid, NEW.motionstreak);
-            END
-        ");
-
-        // Create trigger for UPDATE operation (vice versa)
-        DB::unprepared("
-            CREATE TRIGGER dayalog_devices_update_trigger
-            AFTER UPDATE ON $devicesTable
-            FOR EACH ROW
-            BEGIN
-                UPDATE $tcDevicesTable
-                SET name = NEW.name, uniqueid = NEW.uniqueid, lastupdate = NEW.lastupdate, positionid = NEW.positionid, groupid = NEW.groupid, attributes = NEW.attributes, phone = NEW.phone, model = NEW.model, contact = NEW.contact, category = NEW.category, disabled = NEW.disabled, status = NEW.status, geofenceids = NEW.geofenceids, expirationtime = NEW.expirationtime, motionstate = NEW.motionstate, motiontime = NEW.motiontime, motiondistance = NEW.motiondistance, overspeedstate = NEW.overspeedstate, overspeedtime = NEW.overspeedtime, overspeedgeofenceid = NEW.overspeedgeofenceid, motionstreak = NEW.motionstreak
-                WHERE id = NEW.id;
-            END
-        ");
-
-        // Create trigger for DELETE operation (vice versa)
-        DB::unprepared("
-            CREATE TRIGGER dayalog_devices_delete_trigger
-            AFTER DELETE ON $devicesTable
-            FOR EACH ROW
-            BEGIN
-                DELETE FROM $tcDevicesTable WHERE id = OLD.id;
-            END
-        ");
     }
 
     /**
@@ -106,9 +73,6 @@ return new class extends Migration
         DB::connection('traccar')->unprepared('DROP TRIGGER IF EXISTS tracar_tc_devices_insert_trigger');
         DB::connection('traccar')->unprepared('DROP TRIGGER IF EXISTS tracar_tc_devices_update_trigger');
         DB::connection('traccar')->unprepared('DROP TRIGGER IF EXISTS tracar_tc_devices_delete_trigger');
-        DB::unprepared('DROP TRIGGER IF EXISTS dayalog_devices_insert_trigger');
-        DB::unprepared('DROP TRIGGER IF EXISTS dayalog_devices_update_trigger');
-        DB::unprepared('DROP TRIGGER IF EXISTS dayalog_devices_delete_trigger');
 
         // Drop tables
         Schema::dropIfExists('devices');
