ec9834b631382fba024d711b16491194f7aae246
Ivan Yiga <ivanatresyn@gmail.com>
2023-07-03 08:56:27 +0300

    Added success & error messages

--- resources/js/Pages/AddDevice.vue
+++ resources/js/Pages/AddDevice.vue
@@ -79,6 +79,35 @@
                     </div>
                 </div>
 
+                <div class="rounded-md bg-green-50 p-4 mt-4 border border-green-300">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <CheckCircleIcon class="h-5 w-5 text-green-400" aria-hidden="true" />
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-sm font-medium text-green-800">Successfully created</p>
+                        </div>
+                        <div class="ml-auto pl-3">
+                            <div class="-mx-1.5 -my-1.5">
+                                <button type="button" class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
+                                    <span class="sr-only">Dismiss</span>
+                                    <XMarkIcon class="h-5 w-5" aria-hidden="true" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rounded-md bg-red-50 border border-red-400 p-4 mt-4">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
+                        </div>
+                        <div class="ml-3">
+                            <h3 class="text-sm font-medium text-red-800">There was an error with your submission</h3>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="mt-6 flex items-center justify-end gap-x-6">
                     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save Device</button>
                 </div>
@@ -92,6 +121,8 @@
 
   <script setup>
 import { useForm } from '@inertiajs/vue3';
+import { CheckCircleIcon, XMarkIcon } from '@heroicons/vue/20/solid'
+import { XCircleIcon } from '@heroicons/vue/20/solid'
 
 const form = useForm({
     'name':'',
