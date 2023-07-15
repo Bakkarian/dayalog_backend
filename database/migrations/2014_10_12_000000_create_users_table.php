fa50ca08df6e2725c90c10b10f15db13cd929565
iamafasha <afashaisakiye@gmail.com>
2023-07-15 16:07:29 +0300

    feat: make password nullable to be able to create driver accounts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('patasente_id')->unique()->nullable();
             $table->string('phone_number')->unique()->nullable();
             $table->timestamp('phone_verified_at')->nullable();
