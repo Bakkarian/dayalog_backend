ca5c2654cbe0ec94e4c2dab5d1533dd8b88982e9
iamafasha <afashaisakiye@gmail.com>
2023-07-21 06:46:11 +0300

    feat: add device position model

---
+++ app/Models/DevicePosition.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DevicePosition extends Model
+{
+    use HasFactory;
+
+    protected $table = 'device_positions';
+
+    
+}
