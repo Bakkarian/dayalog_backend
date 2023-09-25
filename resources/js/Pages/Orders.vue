8c882af3f0ee71b9b7121e6941b2146bf6ec66c1
Ivan Yiga <ivanatresyn@gmail.com>
2023-09-25 15:23:07 +0300

    Added create order link

--- resources/js/Pages/Orders.vue
+++ resources/js/Pages/Orders.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6 max-w-7xl">
+        <a href="/create-order" class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mx-auto h-12 w-12 text-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+            </svg>
+
+            <span class="mt-2 block text-sm font-semibold text-gray-900">Create new order</span>
+        </a>
+    </div>
+    <div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6 max-w-7xl">
         <div class="mb-8">
             <h1 class="text-xl">View Orders</h1>
         </div>
@@ -176,8 +185,7 @@
             </div>
         </div>
     </div>
-    <template>
-        <template>
+
             <TransitionRoot as="template" :show="openAssignModal">
                 <Dialog as="div" class="relative z-10" @close="openAssignModal = false">
                     <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
@@ -227,8 +235,6 @@
                     </div>
                 </Dialog>
             </TransitionRoot>
-        </template>
-    </template>
 </template>
 
 <script setup>
