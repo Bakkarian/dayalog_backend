92a1bee1ebb9da23c9e3fe94f276575c595ae082
iamafasha <afashaisakiye@gmail.com>
2023-09-26 10:07:07 +0300

    feat: show actual user name on the profile

--- resources/js/Layouts/ShortLayout.vue
+++ resources/js/Layouts/ShortLayout.vue
@@ -90,9 +90,9 @@
                 <Menu as="div" class="relative">
                   <MenuButton class="-m-1.5 flex items-center p-1.5">
                     <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    <!-- <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" /> -->
                     <span class="hidden lg:flex lg:items-center">
-                    <span class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">Tom Cook</span>
+                    <span class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">{{user.name}}</span>
                     <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
                   </span>
                   </MenuButton>
@@ -149,15 +149,14 @@
   import { Link, usePage } from '@inertiajs/vue3';
   import { computed } from 'vue';
 
+  const page = usePage()
+  const user = computed(() => page.props.auth.user)
+
   const url = computed(() => usePage().url)
   const { navigation, userNavigation } = useNavigation()
 
   const sidebarOpen = ref(false)
   let loadingList = ref(true)
-
-  function getLogo(){
-    return require("./assets/LOGO.png");
-  }
 
   setTimeout(function(){
     let element = document.getElementById("driver-list")
