a282c528521d2f43eaa4d6692560e68aef666c36
Ivan Yiga <ivanatresyn@gmail.com>
2023-09-28 15:41:20 +0300

    Added price field on orders

--- resources/js/Pages/Orders.vue
+++ resources/js/Pages/Orders.vue
@@ -223,6 +223,13 @@
                                                     </div>
                                                 </Combobox>
                                             </div>
+
+                                            <div class="mt-2">
+                                                <label for="deliveryPrice" class="block text-sm font-medium leading-6 text-gray-900">Delivery Price</label>
+                                                <div class="mt-2">
+                                                    <input type="number" name="deliveryPrice" id="deliveryPrice" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="price in UGX" />
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="mt-5 sm:mt-6 flex">
