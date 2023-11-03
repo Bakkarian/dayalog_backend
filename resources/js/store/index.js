ed1658993bc74e400acb2527f14eb32faadf83f0
iamafasha <afashaisakiye@gmail.com>
2023-11-03 17:41:34 +0300

    feat install and setup pinia

---
+++ resources/js/store/index.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'
+export const useMapStore = defineStore('map', () => {
+})
+
+
+// ref()s become state properties
+// computed()s become getters
+// function()s become actions
