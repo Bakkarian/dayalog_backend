27b5c89fdaacfe123c6829a19795e2d9782dee54
iamafasha <afashaisakiye@gmail.com>
2023-11-13 13:24:09 +0300

    feat: make payload nullable

--- database/migrations/2023_09_12_041623_add_payload_column_to_orders_table.php
+++ database/migrations/2023_09_12_041623_add_payload_column_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->json('payload');
+            $table->json('payload')->nullable();
         });
     }
 
