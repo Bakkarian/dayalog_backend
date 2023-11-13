aa2818be562563e3df334287def959c129940cb0
iamafasha <afashaisakiye@gmail.com>
2023-11-13 13:22:39 +0300

    cast items to an array

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,9 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $casts = [
+        'items' => 'array',
+    ];
 
     public function orderVehicles()
     {
