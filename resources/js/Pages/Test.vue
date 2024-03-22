df18e1d0900651cdf17469263393081c78f494a8
iamafasha <afashaisakiye@gmail.com>
2024-03-22 19:43:57 +0300

    fix bug

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -25,9 +25,6 @@ defineProps({
 
     <Head title="Tests Page" />
 
-    <template  #aside>
-
-    </template>
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
     >
