21c35ae2e8e1d7cad428521b541bd72b49f2af5e
iamafasha <afashaisakiye@gmail.com>
2024-03-28 19:10:07 +0300

    add dispatch trait to get stops

---
+++ app/Helpers/Traits/Dispatch.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Helpers\Traits;
+
+trait Dispatch
+{
+
+
+    public function getStops($minSpeed = 1.2, $maxDuration = 2 )
+    {
+
+        $positions = $this->devicePositions;
+        $stops = [];
+        $currentStop = null;
+
+        foreach ($positions as $position) {
+            if ($position['speed'] <= $minSpeed) {
+
+                if ($currentStop === null) {
+
+                    $currentStop = [
+                        'start' => $position,
+                        'end' => $position,
+                        'duration' => 0,
+                    ];
+
+                } else {
+
+                    $currentStop['end'] = $position;
+                    $currentStop['duration'] = strtotime($position['fixtime']) - strtotime($currentStop['start']['fixtime']);
+                }
+            } else {
+
+                if ($currentStop !== null) {
+                    //push the previous stop  to stops if the duration was actually greater
+                    if ($currentStop['duration'] >= $maxDuration) {
+                        $stops[] = $currentStop;
+                    }
+
+                    $currentStop = null;
+                }
+
+            }
+        }
+
+        if ($currentStop !== null && $currentStop['duration'] >= $maxDuration) {
+            $stops[] = $currentStop;
+        }
+
+        return $stops;
+    }
+
+}
--- app/Models/Dispatch.php
+++ app/Models/Dispatch.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use App\Helpers\Traits\Dispatch as DispatchTraits;
 class Dispatch extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, DispatchTraits;
 
     protected $guarded = [];
 
