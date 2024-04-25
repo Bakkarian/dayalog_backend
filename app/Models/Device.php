0cb7e8b73ab358089cbcefefa4eef277d695a3d5
Isakiye Afasha <afashaisakiye@gmail.com>
2024-04-25 19:41:42 +0300

    remove ivan

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -3,11 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
     use HasFactory;
+
+
+      /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::addGlobalScope('removeIvan', function (Builder $builder) {
+            $builder->where('uniqueid', '!=', '038020188471');
+        });
+    }
+
     public function vehicle()
     {
         return $this->hasOneThrough(
