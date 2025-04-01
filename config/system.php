f4e96bd8f61e7dda1c5a88cbda3bd855318d8d2c
Afasha Isakiye <afashaisakiye@gmail.com>
2025-04-01 17:08:02 +0300

    dayalog token

--- config/system.php
+++ config/system.php
@@ -4,6 +4,6 @@ return [
        'binjii' => [
            'name' => 'Binjii',
            'url' => 'https://binjii.com',
-           'token' => env('BINJII_ACCESS_TOKEN'),
+           'token' => env('BINJII_ACCESS_TOKEN', 'b6cdf3b4-2ff3-475f-91a6-794f0e4de513' ),
        ],
 ];
